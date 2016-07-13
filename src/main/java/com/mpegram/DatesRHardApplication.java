2f3f426e8752f647b102ca41b1577f1bce2a7d71
Macon Pegram <mpegram3rd@gmail.com>
2016-07-13 14:57:53 -0400

    Basic Scaffolded project

---
+++ src/main/java/com/mpegram/DatesRHardApplication.java
@@ -0,0 +1,12 @@
+package com.mpegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatesRHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatesRHardApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mpegram/models/Timing.java
@@ -0,0 +1,28 @@
+package com.mpegram.models;
+
+import java.util.Date;
+
+/**
+ * Created by mpegram on 7/13/16.
+ */
+public class Timing {
+
+    private Date start;
+    private Date stop;
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getStop() {
+        return stop;
+    }
+
+    public void setStop(Date stop) {
+        this.stop = stop;
+    }
+}
